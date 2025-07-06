96c4ceb8c35e7faf886d9dd21de3b1daa9bd3673
Pedro <bartolomeip16@gmail.com>
2025-07-06 16:38:27 +0200

    first commit

---
+++ content/index.md
@@ -0,0 +1,9 @@
+---
+title: Cybersecurity, IoT and Computer Science.
+permalink: https://github.com/Pi3dra
+---
+
+This is my personal blog, where I'll write about my personal or professional projects, 
+and any interesting computer science topic I am actually interested in.
+
+My github: https://github.com/Pi3dra
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Bartolomei Pedro",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,29 +23,29 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Poppins",
+        body: "Roboto",
         code: "IBM Plex Mono",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "rgba(230, 230, 230, 0.9 )",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
+          secondary: "#20b2aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "rgba(22, 22, 24, 0.9 )",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
+          secondary: "#20b2aa",//"#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
